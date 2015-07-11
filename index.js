c0dfb97b729bbc52806f7105c88e88fbe1110046
Will Anderson <will@itsananderson.com>
2015-07-11 10:22:58 -0700

    Update url to plain old cascadiajs.com

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-var opener = require("opener");
-var url = "http://bit.ly/cascadiafest";
+var opener = require("open");
+var url = "http://cascadiajs.com/";
 
 console.log("Opening " + url);
 opener(url);
